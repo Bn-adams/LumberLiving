f4b753fbc7e339564bf3448dadc6a2cdd28ec1a0
Bn-adams <Up2063504@myport.ac.uk>
2023-11-21 18:13:47 +0000

    axe not positioned but no animation

---
+++ LumberLiving/Assets/Scipts/Player/SwingAxe.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwingAxe : MonoBehaviour
+{
+    public GameObject axeHolder;
+    
+   
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            axeHolder.GetComponent<Animator>().Play("Take 001");
+        }
+       
+    }
+}
