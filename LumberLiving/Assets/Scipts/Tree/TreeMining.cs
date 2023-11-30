09d346e56312e6e5b26c37291b171d66fef9fe29
Bn-adams <126073556+Bn-adams@users.noreply.github.com>
2023-11-30 14:57:46 +0000

    re do stam bar

--- LumberLiving/Assets/Scipts/Tree/TreeMining.cs
+++ LumberLiving/Assets/Scipts/Tree/TreeMining.cs
@@ -9,29 +9,37 @@ public class TreeMining : MonoBehaviour
     public bool isHarvested = false;
     [SerializeField]
     private GameObject Wood;
+    private float waitTime = 0.5f;
 
     // Start is called before the first frame update
     void Start()
     {
         
     }
-
+    public IEnumerator TreeDestroy()
+    {
+        
+        yield return new WaitForSeconds(waitTime);
+        GameObject.Destroy(gameObject);
+        if (isHarvested)
+        {
+            Vector3 randomPosition = new Vector3(UnityEngine.Random.Range(-2, 2), 0, UnityEngine.Random.Range(-2, 2));
+            Vector3 spawnPosition = randomPosition;
+            GameObject newWood = Instantiate(Wood, transform.parent.position + spawnPosition, Quaternion.identity);
+            canBeHarvested = false;
+        }
+    }
     // Update is called once per frame
     void Update()
     {
         if(canBeHarvested && Input.GetKey(KeyCode.Space))
         {
-            GameObject.Destroy(gameObject);
             isHarvested = true;
-        }
-
-        if (isHarvested)
-        {
-            Vector3 randomPosition = new Vector3(UnityEngine.Random.Range(-2, 2), 0, UnityEngine.Random.Range(-2,2));
-            Vector3 spawnPosition = randomPosition;
-            GameObject newWood = Instantiate(Wood, transform.parent.position + spawnPosition, Quaternion.identity);
+            StartCoroutine(TreeDestroy());
             
         }
+
+       
     
     }
     
